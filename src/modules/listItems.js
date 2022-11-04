2befe2e2828c62bb4426fb37c2d3b5e54c2ef5f2
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2022-11-04 19:05:36 +0300

    ADD changes

--- src/modules/listItems.js
+++ src/modules/listItems.js
@@ -1,17 +1,20 @@
-const URL_API = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
-
-// Lookup full meal details by id
-// https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
-// =================================================
-// List all meals by first letter
-// https://www.themealdb.com/api/json/v1/1/search.php?f=b
-
-const getFoodList = async (URL_API) => {
-  const response = await fetch(URL_API);
-  const json = await response.json();
-  const mealList = json.meals.slice(0, 27);
-
-  return mealList;
-};
-
-export { URL_API, getFoodList };
+const comments = (comm, data) => {
+    comm.innerHTML = '';
+    data.map((datd) => {
+      const dateComment1 = document.createElement('div');
+      dateComment1.setAttribute('class', 'date_comment');
+      const b1 = document.createElement('b');
+      b1.setAttribute('class', 'detail_p');
+      b1.innerText = `${datd.creation_date}`;
+      const p5 = document.createElement('p');
+      p5.setAttribute('class', 'detail_p');
+      p5.innerText = `${datd.username}: ${datd.comment}`;
+      dateComment1.appendChild(b1);
+      dateComment1.appendChild(p5);
+      comm.appendChild(dateComment1);
+  
+      return null;
+    });
+  };
+  
+  export default comments;
