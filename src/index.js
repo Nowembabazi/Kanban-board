6eee7415e879cbf1e07c4f835b822cf9e00c27c1
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2022-11-04 13:29:55 +0100

    update the styling

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 import './style.css';
 import showMeals from './modules/showMeals.js';
-import { addLike } from './modules/fetchLikes.js';
-import showlike from './modules/showLike.js';
-import mealsCounter from './modules/mealsCounter.js';
+import populateComment from './modules/populateComment.js';
+
+const urls = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/N317ounBUtSwOefLVAgO/comments';
 
 // Constants
 const mealsListContainer = document.querySelector('.f-list');
+const mealsNav = document.querySelector('.control');
 
 // Populate the meals cards items
-document.addEventListener('DOMContentLoaded', async () => {
-  await showMeals(mealsListContainer);
-  // Selecet meals items
-  const meals = document.getElementsByClassName('meal-card');
-  mealsCounter([...meals]);
-  // Select all likes element
-  const textLikes = document.getElementsByClassName('card-likes-txt');
-  [...textLikes].forEach(async (textLike) => {
-    await showlike(textLike);
-  });
+document.addEventListener('DOMContentLoaded', showMeals(mealsListContainer, 0));
+
+// Populate according to the meals-nav
+mealsNav.addEventListener('click', (e) => {
+  e.preventDefault();
+  // Get the clicked nav link index data.
+  const navIndex = e.target.getAttribute('data-index');
+  // Empty the list meals container from the previous cards
+  mealsListContainer.innerHTML = '';
+  // Generate new 9 items
+  showMeals(mealsListContainer, navIndex);
 });
 
-// Post Likes
-window.addEventListener('click', async (e) => {
-  // Select the like button
-  const likeBtn = e.target;
-  if (likeBtn.classList.contains('card-likes')) {
-    // Update numbers of likes on the API
-    const mealId = likeBtn.getAttribute('data-id');
-    await addLike(mealId);
-    // Update numbers of likes on the screen
-    const txtlike = likeBtn.nextElementSibling;
-    await showlike(txtlike);
-  }
-});
+const handleCommentClick = async (meals) => {
+  const [id, name, category, image, origin] = meals;
+  await populateComment(id, name, category, image, origin, urls);
+  const thepopup = document.getElementById('popup');
+  document.body.classList.add('body-y-scroll');
+  thepopup.classList.add('popup2');
+};
+
+// event.preventDefault();
+window.handleCommentClick = handleCommentClick;
